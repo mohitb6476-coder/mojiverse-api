cae0b762ec15c1534e61c1e4e98ce84078d61e47
Mohit Bhagat <mohitb6476@gmail.com>
2026-03-28 12:33:00 +0530

    Fix literal bash escape string in port mapping

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Fallback to 80 if PORT isn't provided
-PORT=\${PORT:-80}
+PORT=${PORT:-80}
 
 # Update Apache configuration files with the dynamic PORT
 sed -i "s/80/$PORT/g" /etc/apache2/sites-available/000-default.conf /etc/apache2/ports.conf
