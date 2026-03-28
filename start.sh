97b5600f6f161ffc758bab82e47af2a458999da3
Mohit Bhagat <mohitb6476@gmail.com>
2026-03-28 12:47:56 +0530

    Fix migration failure by granting permissions before running migrate

--- start.sh
+++ start.sh
@@ -9,12 +9,12 @@ sed -i "s/80/$PORT/g" /etc/apache2/sites-available/000-default.conf /etc/apache2
 export LOG_CHANNEL=stderr
 export APP_DEBUG=true
 
-# Execute migrations as www-data to ensure the SQLite database doesn't get locked by root!
-su -s /bin/bash -c "php artisan migrate --force" www-data
-
-# Explicitly guarantee storage and database folders are writable by Apache
+# Explicitly guarantee storage and database folders are writable by Apache FIRST
 chown -R www-data:www-data /var/www/html/storage /var/www/html/bootstrap/cache /var/www/html/database
 chmod -R 775 /var/www/html/storage /var/www/html/bootstrap/cache /var/www/html/database
+
+# NOW execute migrations as www-data to ensure the SQLite database doesn't get locked by root!
+su -s /bin/bash -c "php artisan migrate --force" www-data
 
 # Start Apache in foreground
 docker-php-entrypoint apache2-foreground
